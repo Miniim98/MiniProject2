8960411a376d9a2ed083f63bd49be7d77e2f0e86
Amalie <amdh@itu.dk>
2021-10-26 12:27:30 +0200

    update

--- Server/main.go
+++ Server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
 
-func main(){
-	fmt.Println("I am a server")
+	pb "github.itu.dk/jard/Miniproject2.git/Chat"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	string net.UnknownNetworkError
+	string port
+	pb.UnimplementedChittychatServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8008")
+	if err != nil {
+		log.Fatalf("failed to listen on port 8008: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	ft.RegisterTimeServiceServer(grpcServer, &server{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC server over port 8008: %v", err)
+
+	}
 }
